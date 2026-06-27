c2d04224ace1aeb03584528e17a64729b1911303
ruthedias <ellendiasdasilvaruth@gmail.com>
2026-06-27 20:35:40 -0300

    config: adição de annotation EnableWebSecurity e liberar rotas do swagger e users

--- src/main/java/com/ruth/printflow/config/SecurityConfig.java
+++ src/main/java/com/ruth/printflow/config/SecurityConfig.java
@@ -1,4 +1,35 @@
 package com.ruth.printflow.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
+                        //.requestMatchers("/users/register").permitAll()
+                        //.anyRequest().authenticated()
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
