b71a8f508d2afb5b15293470cdac7cb365d27a1b
ruthedias <ellendiasdasilvaruth@gmail.com>
2026-06-27 15:25:05 -0300

    Primeiro Commit

---
+++ src/main/java/com/ruth/printflow/PrintFlowApplication.java
@@ -0,0 +1,13 @@
+package com.ruth.printflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruth/printflow/PrintFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruth.printflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
