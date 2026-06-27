3ecdd4bee7114e897e486aa67e920d04db542025
ruthedias <ellendiasdasilvaruth@gmail.com>
2026-06-27 20:34:51 -0300

    test: adição de teste unitario para o fluxo de registro de usuario

--- src/test/java/com/ruth/printflow/controller/UserControllerTest.java
+++ src/test/java/com/ruth/printflow/controller/UserControllerTest.java
@@ -1,4 +1,54 @@
 package com.ruth.printflow.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ruth.printflow.entity.User;
+import com.ruth.printflow.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void shouldRegisterUser() throws Exception {
+        User request = new User();
+        request.setName("Ruth");
+        request.setEmail("ruth@gmail.com");
+        request.setPassword("123456");
+
+        User response = new User();
+        response.setId(1L);
+        response.setName("Ruth");
+        response.setEmail("ruth@gmail.com");
+        response.setPassword("senha-criptografada");
+
+        when(userService.register(any(User.class))).thenReturn(response);
+
+        mockMvc.perform(post("/users/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Ruth"))
+                .andExpect(jsonPath("$.email").value("ruth@gmail.com"));
+    }
 }
