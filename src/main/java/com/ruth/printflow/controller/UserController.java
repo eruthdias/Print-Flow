a3bdc5b7cbbbb870468cd3e7d6b2b03ecea10930
ruthedias <ellendiasdasilvaruth@gmail.com>
2026-06-27 20:36:22 -0300

    feat: implementação do UserController com endpoint de cadastro e ajuste para o swagger

--- src/main/java/com/ruth/printflow/controller/UserController.java
+++ src/main/java/com/ruth/printflow/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.ruth.printflow.controller;
 
+import com.ruth.printflow.entity.User;
+import com.ruth.printflow.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User savedUser = service.register(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
 }
