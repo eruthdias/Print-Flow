7558dab2901123ea1fb6eb625695b9272d20d135
ruthedias <ellendiasdasilvaruth@gmail.com>
2026-06-27 18:05:10 -0300

    feat: implementação de codificação de senha usando passwordEncoder

--- src/main/java/com/ruth/printflow/service/UserService.java
+++ src/main/java/com/ruth/printflow/service/UserService.java
@@ -2,21 +2,28 @@ package com.ruth.printflow.service;
 
 import com.ruth.printflow.entity.User;
 import com.ruth.printflow.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User register(User user) {
-        if (repository.count()>0){
+        if (repository.count() > 0) {
             throw new RuntimeException("Já existe um usuário cadastrado");
         }
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         return repository.save(user);
     }
 }
